698c589eb2042c73739b8f36b0c8af5319b97206
Akhtaruzzaman <akhtr01947@gmail.com>
2021-08-24 11:12:25 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".testimonial_wrap").owlCarousel({
+    loop: true,
+    items: 1,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    dots: true,
+  });
+});
